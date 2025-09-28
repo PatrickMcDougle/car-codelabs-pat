196ee20babe064c625773895b5a9eaf944346e45
Patrick McDougle <Patrick.McDougle@gmail.com>
2025-09-28 13:49:48 -0400

    CarAppService creation
    Step 5 complete.

--- common/car-app-service/src/main/java/com/example/places/carappservice/PlacesCarAppService.kt
+++ common/car-app-service/src/main/java/com/example/places/carappservice/PlacesCarAppService.kt
@@ -1,4 +1,28 @@
 package com.example.places.carappservice
 
-class PlacesCarAppService {
+import android.content.Intent
+import androidx.car.app.CarAppService
+import androidx.car.app.Screen
+import androidx.car.app.Session
+import androidx.car.app.SessionInfo
+import androidx.car.app.validation.HostValidator
+import com.example.places.carappservice.screen.MainScreen
+
+class PlacesCarAppService : CarAppService() {
+    override fun createHostValidator(): HostValidator {
+        return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
+    }
+
+    override fun onCreateSession(sessionInfo: SessionInfo): Session {
+        return PlacesSession()
+//        return super.onCreateSession(sessionInfo)
+    }
+}
+
+class PlacesSession : Session() {
+    override fun onCreateScreen(intent: Intent): Screen {
+        // MainScreen will be an unresolved reference until the next step
+        return MainScreen(carContext)
+    }
+
 }
